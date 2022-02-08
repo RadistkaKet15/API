5335764f544334d42b47777561fb3c9f3b4e803a
Ijne <87993963+Ijne@users.noreply.github.com>
2022-02-08 19:07:39 +0300

    5 ура

--- Maps1.py
+++ Maps1.py
@@ -18,12 +18,19 @@ class Example(QWidget):
         self.l_map = 'map'
         self.format_map = "png"
         self.geocode = 'Москва'
-        self.getImage(self.spn, self.coord_x, self.coord_y, self.l_map)
+        self.geocode_x = 0
+        self.geocode_y = 0
+        self.metka = 0
+        self.getImage(self.spn, self.coord_x, self.coord_y, self.l_map,
+                      self.geocode_x, self.geocode_y)
+
+        self.setMouseTracking(True)
+
         self.initUI()
 
-    def getImage(self, spn, coord_x, coord_y, l_map):
+    def getImage(self, spn, coord_x, coord_y, l_map, geocode_x, geocode_y):
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={coord_x}," \
-                      f"{coord_y}&spn={spn},{spn}&l={l_map}"
+                      f"{coord_y}&spn={spn},{spn}&l={l_map}&pt={geocode_x},{geocode_y},flag"
         response = requests.get(map_request)
 
         if not response:
@@ -61,6 +68,7 @@ class Example(QWidget):
         self.hybrid.move(500, 120)
 
         self.line = QLineEdit(self)
+        self.line.setFocusPolicy(Qt.ClickFocus)
         self.line.move(20, 20)
 
         self.find_it = QPushButton('Искать', self)
@@ -68,21 +76,33 @@ class Example(QWidget):
         self.find_it.clicked.connect(self.find_it_func)
         self.find_it.move(20, 50)
 
+    def mousePressEvent(self, event):
+        pass
+
+    def mouseReleaseEvent(self, event):
+        pass
+
     def find_it_func(self):
-        self.geocode = self.line.text()
-        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-        geocoder_params = {
-            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-            "geocode": self.geocode,
-            "format": "json"}
-        response = requests.get(geocoder_api_server, params=geocoder_params)
-        json_response = response.json()
-        toponym = json_response["response"]["GeoObjectCollection"][
-            "featureMember"][0]["GeoObject"]
-        toponym_coodrinates = toponym["Point"]["pos"]
-        self.coord_x = toponym_coodrinates[0]
-        self.coord_y = toponym_coodrinates[1]
-        self.run()
+        self.line.setEnabled(False)
+        self.line.setEnabled(True)
+        try:
+            self.geocode = self.line.text()
+            geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+            geocoder_params = {
+                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                "geocode": self.geocode,
+                "format": "json"}
+            response = requests.get(geocoder_api_server, params=geocoder_params)
+            json_response = response.json()
+            toponym = json_response["response"]["GeoObjectCollection"][
+                "featureMember"][0]["GeoObject"]
+            toponym_coodrinates = toponym["Point"]["pos"].split()
+            self.geocode_x = self.coord_x = float(toponym_coodrinates[0])
+            self.geocode_y = self.coord_y = float(toponym_coodrinates[1])
+            print(self.coord_x, self.coord_y)
+            self.run()
+        except Exception:
+            print('WTF bro?!')
 
     def checked(self):
         signal = self.sender()
@@ -130,7 +150,8 @@ class Example(QWidget):
                 self.run()
 
     def run(self):
-        self.getImage(self.spn, self.coord_x, self.coord_y, self.l_map)
+        self.getImage(self.spn, self.coord_x, self.coord_y, self.l_map,
+                      self.geocode_x, self.geocode_y)
         self.pixmap = QPixmap(self.map_file)
         self.image.setPixmap(self.pixmap)
 
