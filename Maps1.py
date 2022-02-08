9d483b3f6ee6a5b5780d2ae1120971adaa52d866
Ekaterina <ekaterina.gladilkina@yandex.ru>
2022-02-08 17:31:22 +0300

    4

--- Maps1.py
+++ Maps1.py
@@ -15,12 +15,13 @@ class Example(QWidget):
         self.spn = 0.01
         self.coord_x = 37.530887
         self.coord_y = 55.703118
-        self.getImage(self.spn, self.coord_x, self.coord_y)
+        self.l_map = 'map'
+        self.getImage(self.spn, self.coord_x, self.coord_y, self.l_map)
         self.initUI()
 
-    def getImage(self, spn, coord_x, coord_y):
+    def getImage(self, spn, coord_x, coord_y, l_map):
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={coord_x}," \
-                      f"{coord_y}&spn={spn},{spn}&l=map"
+                      f"{coord_y}&spn={spn},{spn}&l={l_map}"
         response = requests.get(map_request)
 
         if not response:
@@ -42,6 +43,30 @@ class Example(QWidget):
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
 
+        self.scheme = QPushButton('Схема', self)
+        self.scheme.clicked.connect(self.checked)
+        self.scheme.move(500, 20)
+
+        self.satellite = QPushButton('Спутник', self)
+        self.satellite.clicked.connect(self.checked)
+        self.satellite.move(500, 70)
+
+        self.hybrid = QPushButton('Гибрид', self)
+        self.hybrid.clicked.connect(self.checked)
+        self.hybrid.move(500, 120)
+
+    def checked(self):
+        signal = self.sender()
+        if signal == self.scheme:
+            self.l_map = "map"
+            self.run()
+        elif signal == self.satellite:
+            self.l_map = "sat"
+            self.run()
+        elif signal == self.hybrid:
+            self.l_map = "sat,skl"
+            self.run()
+
     def closeEvent(self, event):
         os.remove(self.map_file)
 
@@ -50,39 +75,32 @@ class Example(QWidget):
         if key == Qt.Key_PageUp:
             if self.spn < 5.0:
                 self.spn += 0.1
-                self.getImage(self.spn, self.coord_x, self.coord_y)
-                self.pixmap = QPixmap(self.map_file)
-                self.image.setPixmap(self.pixmap)
+                self.run()
         elif key == Qt.Key_PageDown:
             if self.spn > 0.1:
                 self.spn -= 0.1
-                self.getImage(self.spn, self.coord_x, self.coord_y)
-                self.pixmap = QPixmap(self.map_file)
-                self.image.setPixmap(self.pixmap)
+                self.run()
         elif key == Qt.Key_Up:
             if self.coord_y < 84:
                 self.coord_y += 0.1
-                self.getImage(self.spn, self.coord_x, self.coord_y)
-                self.pixmap = QPixmap(self.map_file)
-                self.image.setPixmap(self.pixmap)
+                self.run()
         elif key == Qt.Key_Down:
             if self.coord_y > -84:
                 self.coord_y -= 0.1
-                self.getImage(self.spn, self.coord_x, self.coord_y)
-                self.pixmap = QPixmap(self.map_file)
-                self.image.setPixmap(self.pixmap)
+                self.run()
         elif key == Qt.Key_Right:
             if self.coord_x < 179:
                 self.coord_x += 0.1
-                self.getImage(self.spn, self.coord_x, self.coord_y)
-                self.pixmap = QPixmap(self.map_file)
-                self.image.setPixmap(self.pixmap)
+                self.run()
         elif key == Qt.Key_Left:
             if self.coord_x > -179:
                 self.coord_x -= 0.1
-                self.getImage(self.spn, self.coord_x, self.coord_y)
-                self.pixmap = QPixmap(self.map_file)
-                self.image.setPixmap(self.pixmap)
+                self.run()
+
+    def run(self):
+        self.getImage(self.spn, self.coord_x, self.coord_y, self.l_map)
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
 
 
 if __name__ == '__main__':
